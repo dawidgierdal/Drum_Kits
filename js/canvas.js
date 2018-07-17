d42c8b6ae6a7a2e5c62e82221eaf05034c8121fa
Your Name <you@example.com>
2018-07-17 10:45:56 +0200

    Sections, style, canvas, audio

---
+++ js/canvas.js
@@ -0,0 +1,96 @@
+let canvas = document.querySelector('canvas');
+let c = canvas.getContext('2d');
+let mouse = {
+    x: undefined,
+    y: undefined
+};
+let colorAray = ['rgba(145, 52, 52, 0.7)', 'rgba(52, 145, 131, 0.7)', 'rgba(52, 127, 145, 0.7)', 'rgba(92, 52, 145, 0.7)', 'rgba(135, 52, 145, 0.7)'];
+
+let maxRadius = 40;
+let minRadius = 3;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+canvas.addEventListener('mousemove', function (event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    console.log(event)
+});
+
+window.addEventListener('resize', function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    init();
+});
+
+
+function Circle(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.color = colorAray[Math.floor(Math.random() * colorAray.length)];
+
+    this.draw = function () {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+    };
+
+    this.update = function () {
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+
+        //Interactivity
+        if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+
+            if (this.radius < maxRadius) {
+                this.radius += 2;
+            }
+        } else if (this.radius > minRadius) {
+            this.radius -= 2;
+        }
+
+        this.draw();
+    }
+}
+
+let circleArray = [];
+
+function init() {
+
+    circleArray = [];
+    for (let i = 0; i < 800; i++) {
+        let radius = Math.random() * 3 + 1;
+        let x = Math.random() * (innerWidth - radius * 2) + radius;
+        let y = Math.random() * (innerHeight - radius * 2) + radius;
+        let dx = (Math.random() - 0.5) * 2;
+        let dy = (Math.random() - 0.5) * 2;
+        circleArray.push(new Circle(x, y, dx, dy, radius));
+    }
+}
+
+init();
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerWidth);
+
+    for (let i = 0; i < circleArray.length; i++) {
+        circleArray[i].update();
+    }
+}
+
+animate();
+
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+var path = require("path");
+var Html = require('html-webpack-plugin');
+var MiniCSS = require("mini-css-extract-plugin");
+
+
+module.exports = function(env) {
+    const isDev = env && env.dev ? true : false;
+    console.log(isDev, 'isDev');
+
+    const config = {
+        entry: "./js/app.js",
+        output: {
+            filename: "out.js",
+            path: path.resolve(__dirname, "build")
+        },
+        mode: isDev ? 'development' : 'production',
+        module: {
+            rules: [
+                {
+                    test: /\.jsx$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015', "stage-2", "react"]
+                        }
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        isDev ? 'style-loader' : MiniCSS.loader,
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        isDev ? 'style-loader' : MiniCSS.loader,
+                        'css-loader',
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: () => [
+                                    new require('autoprefixer')({
+                                        browsers: [
+                                            'ie 11' // tu definiujemy wsparcie dla przegladarek w css
+                                        ]
+                                    })
+                                ]
+                            }
+                        },
+                        'sass-loader'
+                    ]
+                },
+                {
+                    test: /\.(jpg|jpeg|gif|png|csv)$/,
+                    use: {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            publicPath: 'images',
+                            outputPath: 'images'
+                        }
+                    }
+                },
+                {
+                    test: /\.(eot|ttf|woff|woff2)$/,
+                    use: {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            publicPath: 'fonts',
+                            outputPath: 'fonts'
+                        }
+                    }
+                }
+            ]
+        },
+        plugins: [
+            new Html({
+                filename: 'index.html',
+                template: './index.html'
+            }),
+            new MiniCSS({
+                filename: "app.css", // definiujemy adres pliku css
+            })
+        ]
+    }
+
+    return config;
+}
