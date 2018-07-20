01e869a6bbe1e63f38d90838a8953ed5e00ef357
Your Name <you@example.com>
2018-07-20 11:40:53 +0200

    docs

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = function(env) {
         entry: "./js/main.jsx",
         output: {
             filename: "out.js",
-            path: path.resolve(__dirname, "build")
+            path: path.resolve(__dirname, "docs")
         },
         mode: isDev ? 'development' : 'production',
         module: {
